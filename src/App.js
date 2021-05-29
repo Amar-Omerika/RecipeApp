af6549d2bfd4ac3a7406489c0d3a2208e6b874c9
Amar Omerika <amar.omerika@edu.fit.ba>
2021-05-29 23:20:30 +0200

    recipe-app

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 const App = () => {
-	const App_ID = "5352863c";
-	const App_KEY = "33bbcd330b2eb89dc2969873eba80d14";
+	const APP_ID = "5352863c";
+	const APP_KEY = "33bbcd330b2eb89dc2969873eba80d14";
+	const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
 	return (
 		<div className="App">
 			<h1>React hello</h1>
