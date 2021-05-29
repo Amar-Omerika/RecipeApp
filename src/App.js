941c76e1a572b9a765059e7ff1bd271f6810cdea
Amar Omerika <amar.omerika@edu.fit.ba>
2021-05-29 23:15:38 +0200

    recipe-app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-
-function App() {
+const App = () => {
+	const App_ID = "5352863c";
+	const App_KEY = "33bbcd330b2eb89dc2969873eba80d14";
 	return (
 		<div className="App">
-			<h1>Hello React</h1>
+			<h1>React hello</h1>
 		</div>
 	);
-}
+};
 
 export default App;
