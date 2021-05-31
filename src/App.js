9770b3151f8205ffab69d2fcc71fffbc72fefb89
Amar Omerika <amar.omerika@edu.fit.ba>
2021-05-31 20:26:56 +0200

    recipe-app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React, { useEffect, useState } from "react";
 import Recipe from "./Recipe";
 import "./App.css";
+
 const App = () => {
 	const APP_ID = "5352863c";
 	const APP_KEY = "33bbcd330b2eb89dc2969873eba80d14";
+
 	const [recipes, setRecipes] = useState([]);
 
 	useEffect(() => {
 		getRecipes();
 	}, []);
+
 	const getRecipes = async () => {
 		const response = await fetch(
 			`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
 		);
 		const data = await response.json();
 		setRecipes(data.hits);
+		console.log(data.hits);
 	};
 
 	return (
@@ -25,6 +29,13 @@ const App = () => {
 					Search
 				</button>
 			</form>
+			{recipes.map((recipe) => (
+				<Recipe
+					title={recipe.recipe.lable}
+					calories={recipe.recipe.calories}
+					image={recipe.recipe.image}
+				/>
+			))}
 		</div>
 	);
 };
